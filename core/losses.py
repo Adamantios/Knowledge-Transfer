cf07e674cbd3db8617f3ef6d1e50b536cbd08118
Adamantios <manoszaras94@gmail.com>
2019-07-23 12:25:10 +0300

    Add docstring and increase code readability.

--- core/losses.py
+++ core/losses.py
@@ -23,7 +23,7 @@ def supervised_loss(y_true: Tensor, y_pred: Tensor, lambda_const: float) -> Tens
 def distillation_loss(teacher_logits: Tensor, student_logits: Tensor, temperature: float,
                       lambda_const: float) -> Tensor:
     """
-    Calculates the Distillation Loss between two networks logits.
+    Calculates the Distillation Loss between two networks' logits.
 
     :param teacher_logits: the teacher network's logits.
     :param student_logits: the student network's logits.
@@ -37,17 +37,24 @@ def distillation_loss(teacher_logits: Tensor, student_logits: Tensor, temperatur
     return loss + supervised_loss(y_teacher, y_student, lambda_const)
 
 
-def pkt_loss(y_true: Tensor, y_pred: Tensor) -> Tensor:
+def pkt_loss(y_teacher: Tensor, y_student: Tensor) -> Tensor:
+    """
+    Calculates the Probabilistic Knowledge Transfer Loss between two networks.
+
+    :param y_teacher: the teacher's values.
+    :param y_student: the student's values.
+    :return: the transfer loss.
+    """
     # Calculate the cosine similarities.
     # TODO check if .T is the same as .transpose(0, 1).
-    y_true = dot((y_true, y_true.transpose(0, 1)), l2_normalize=True)
-    y_pred = dot((y_pred, y_pred.transpose(0, 1)), l2_normalize=True)
+    y_teacher = dot((y_teacher, y_teacher.transpose(0, 1)), l2_normalize=True)
+    y_student = dot((y_student, y_student.transpose(0, 1)), l2_normalize=True)
 
     # Transform them into probabilities.
-    y_true = y_true / sum(y_true, dim=1, keepdim=True)
-    y_pred = y_pred / sum(y_pred, dim=1, keepdim=True)
+    y_teacher = y_teacher / sum(y_teacher, dim=1, keepdim=True)
+    y_student = y_student / sum(y_student, dim=1, keepdim=True)
 
     # Calculate the KL-divergence.
-    loss = kullback_leibler_divergence(y_true, y_pred)
+    loss = kullback_leibler_divergence(y_teacher, y_student)
 
     return loss
