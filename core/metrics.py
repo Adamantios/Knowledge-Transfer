f5e65dcd19fbc9fe27d624a1db73ed99d6fd6921
Adamantios <manoszaras94@gmail.com>
2019-09-14 12:58:37 +0300

    Correct _pkt_loss_calculator.

--- core/metrics.py
+++ core/metrics.py
@@ -1,6 +1,7 @@
+from itertools import combinations_with_replacement
 from typing import Callable, Tuple
 
-from tensorflow import Tensor
+from tensorflow import Tensor, zeros
 from tensorflow.python.keras.backend import dot, sum, identity
 from tensorflow.python.keras.losses import categorical_crossentropy, kullback_leibler_divergence
 from tensorflow.python.ops.math_ops import divide, multiply, add
@@ -44,6 +45,7 @@ def _distillation_loss_calculator(teacher_logits: Tensor, student_logits: Tensor
     :param y_true: the true labels, if performing supervised distillation.
     :param y_pred: the predicted labels, if performing supervised distillation.
     :param lambda_const: the importance weight of the supervised loss.
+    Set it to 0 if you do not want to apply supervised loss.
     :return: the distillation loss.
     """
     # Apply softmax with temperature to the teacher's logits.
@@ -81,25 +83,48 @@ def distillation_loss(temperature: float, lambda_const: float) -> LossType:
     return distillation
 
 
-def _pkt_loss_calculator(y_teacher: Tensor, y_student: Tensor) -> Tensor:
+def _pkt_loss_calculator(y_teacher: Tensor, y_student: Tensor, y_true: Tensor, y_pred: Tensor,
+                         lambda_const: float) -> Tensor:
     """
     Calculates the Probabilistic Knowledge Transfer Loss between two networks.
 
     :param y_teacher: the teacher's values.
     :param y_student: the student's values.
+    :param y_true: the true labels, if performing supervised distillation.
+    :param y_pred: the predicted labels, if performing supervised distillation.
+    :param lambda_const: the importance weight of the supervised loss.
+    Set it to 0 if you do not want to apply supervised loss.
     :return: the probabilistic knowledge transfer loss.
     """
-    # Calculate the cosine similarities.
-    # TODO check if .T is the same as .transpose(0, 1).
-    y_teacher = dot((y_teacher, y_teacher.transpose(0, 1)), l2_normalize=True)
-    y_student = dot((y_student, y_student.transpose(0, 1)), l2_normalize=True)
 
-    # Transform them into probabilities.
-    y_teacher = y_teacher / sum(y_teacher, dim=1, keepdim=True)
-    y_student = y_student / sum(y_student, dim=1, keepdim=True)
+    def cosine_similarity(tensor: Tensor) -> Tensor:
+        """ Calculates the cosine similarity of a 2D array. """
+        return dot((tensor, tensor.T), l2_normalize=True)
 
-    # Calculate the KL-divergence.
-    loss = kullback_leibler_divergence(y_teacher, y_student)
+    def to_probabilities(tensor: Tensor):
+        """ Transforms a symmetric 2D array's values into probabilities. """
+        return tensor / sum(tensor, axis=1, keepdims=True)
+
+    teacher_similarity, student_similarity = cosine_similarity(y_teacher), cosine_similarity(y_student)
+    teacher_similarity, student_similarity = to_probabilities(teacher_similarity), to_probabilities(student_similarity)
+    loss = kullback_leibler_divergence(teacher_similarity, student_similarity)
+
+    # If supervised distillation is being performed.
+    if lambda_const:
+        # Initialize symmetric supervised similarity matrix targets.
+        target_similarity = zeros(y_true.shape[0], y_true.shape[0])
+        # Run through all the target index combinations, without duplicates.
+        for i, j in combinations_with_replacement(y_true.shape[0], 2):
+            # If samples have the same target, make symmetric similarity 1.
+            if y_true[i] == y_true[j]:
+                target_similarity[i, j] = target_similarity[j, i] = 1
+
+        predicted_similarity = cosine_similarity(y_pred)
+        target_similarity = to_probabilities(target_similarity)
+        predicted_similarity = to_probabilities(predicted_similarity)
+
+        # Add supervised loss, multiplied by its importance weight.
+        add(loss, multiply(lambda_const, kullback_leibler_divergence(target_similarity, predicted_similarity)))
 
     return loss
 
@@ -121,12 +146,7 @@ def pkt_loss(lambda_const: float) -> LossType:
         :return: the probabilistic knowledge transfer loss.
         """
         teacher_logits, student_logits, y_true, y_pred = _split_targets(y_true, y_pred, bool(lambda_const))
-        loss = _pkt_loss_calculator(teacher_logits, student_logits)
-
-        if lambda_const:
-            add(loss, multiply(lambda_const, _pkt_loss_calculator(y_true, y_pred)))
-
-        return loss
+        return _pkt_loss_calculator(teacher_logits, student_logits, y_true, y_pred, lambda_const)
 
     return pkt
 
