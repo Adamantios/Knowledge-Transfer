dda1af422cbfefa9e4935711e2ede4dbb6b50c86
Adamantios <manoszaras94@gmail.com>
2019-09-30 16:31:13 +0300

    Get scores correctly.

--- utils/helpers.py
+++ utils/helpers.py
@@ -214,7 +214,7 @@ def _get_model_results(scores: list, metrics_names: list) -> str:
     """
     results = ''
     for i in range(len(scores)):
-        results += "{}: {}\n".format(metrics_names[i], scores[1])
+        results += "{}: {}\n".format(metrics_names[i], scores[i])
 
     return results
 
