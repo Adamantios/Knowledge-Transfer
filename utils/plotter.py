c4a70491ec6ab123d4fa997233acc96453ddf6e1
Adamantios <manoszaras94@gmail.com>
2019-10-09 10:43:44 +0300

    Plot comparison only if more than one methods are being used.

--- utils/plotter.py
+++ utils/plotter.py
@@ -34,32 +34,34 @@ def plot_results(results: List[Dict], epochs: int, save_folder: Optional[str]) -
                         filepath = join(save_folder, result['method'] + '_' + metric + '_vs_epoch' + '.png')
                         fig.savefig(filepath)
 
-    # Plot KT methods comparison for each metric.
-    linestyles = ['--', '-.', ':']
-    i = 0
-    for metric_index, metric in enumerate(results[0]['history'].keys()):
-        # Plot only validation metric results.
-        if metric.startswith('val_') and 'loss' not in metric:
-            i += 1
-            linestyles_pool = cycle(linestyles)
-            # Create subplot for overall KT methods comparison for the current metric.
-            fig, ax = plt.subplots(figsize=(12, 10))
-            ax.set_title('KT Methods Comparison', fontsize='x-large')
-            ax.set_xlabel('epoch', fontsize='large')
-            ax.set_ylabel(metric, fontsize='large')
-            # For every method.
-            for result in results:
-                if result['method'] == 'Teacher':
-                    # Plot teacher baseline.
-                    baseline = asarray([result['evaluation'][i] for _ in range(epochs)])
-                    ax.plot(baseline, label=result['method'], linestyle='-')
-                else:
-                    # Plot method's current metric results.
-                    ax.plot(list(result['history'].values())[metric_index], label=result['method'],
-                            linestyle=next(linestyles_pool))
+    # Plot KT methods comparison for each metric, if there are more than 1 methods.
+    # Using > 2, because the results contain the teacher too.
+    if len(results) > 2:
+        linestyles = ['--', '-.', ':']
+        i = 0
+        for metric_index, metric in enumerate(results[0]['history'].keys()):
+            # Plot only validation metric results.
+            if metric.startswith('val_') and 'loss' not in metric:
+                i += 1
+                linestyles_pool = cycle(linestyles)
+                # Create subplot for overall KT methods comparison for the current metric.
+                fig, ax = plt.subplots(figsize=(12, 10))
+                ax.set_title('KT Methods Comparison', fontsize='x-large')
+                ax.set_xlabel('epoch', fontsize='large')
+                ax.set_ylabel(metric, fontsize='large')
+                # For every method.
+                for result in results:
+                    if result['method'] == 'Teacher':
+                        # Plot teacher baseline.
+                        baseline = asarray([result['evaluation'][i] for _ in range(epochs)])
+                        ax.plot(baseline, label=result['method'], linestyle='-')
+                    else:
+                        # Plot method's current metric results.
+                        ax.plot(list(result['history'].values())[metric_index], label=result['method'],
+                                linestyle=next(linestyles_pool))
 
-            ax.legend(loc='best', fontsize='large')
-            plt.show()
-            if save_folder is not None:
-                filepath = join(save_folder, 'KT_Methods_Comparison_' + metric + '_vs_epoch' + '.png')
-                fig.savefig(filepath)
+                ax.legend(loc='best', fontsize='large')
+                plt.show()
+                if save_folder is not None:
+                    filepath = join(save_folder, 'KT_Methods_Comparison_' + metric + '_vs_epoch' + '.png')
+                    fig.savefig(filepath)
